298c7bb12248c32527ff0d21a43a604e47a132f3
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-18 09:22:59 +0200

    25:46

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("Codul a fost executat")
+// console.warn("Codul a fost executat")
+// console.error("Codul a fost executat")
+// var, let, const
+var myName="Bogdan"; //variabila globala, de evitat
+let secondName="Mihai"; //variabila locala, recomandat
+const age=30
+// min 25:46
+
+
+
+
+
+
