eb380881c22dc9588b73bdd3ea90964e70c7c2db
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-18 11:07:13 +0200

    operatori

--- index.js
+++ index.js
@@ -2,11 +2,72 @@
 // console.warn("Codul a fost executat")
 // console.error("Codul a fost executat")
 // var, let, const
-var myName="Bogdan"; //variabila globala, de evitat
-let secondName="Mihai"; //variabila locala, recomandat
-const age=30
-// min 25:46
+// //PRIMITIVE
+// var myName="Bogdan"; //variabila globala, de evitat
+// let secondName="Mihai"; //variabila locala, recomandat
+// const age=30;
+// let boolean=false;
+// let n=null; //referinta catre o valoare inexistenta
+// let undef=undefined; //valoare nedefinita
+// console.log(typeof(boolean));
+// console.log(typeof(undef));
+// let str='insireuire de caractere';
+// console.log(typeof(str));
+// //OBIECT -> object literal
+// let person={
+//     nume:"Daniel",
+//     age:20,
+//     adresa:"Romania"
+// }
+// let person1={
+//     nume:"Daniel",
+//     age:20,
+//     adresa:"Romania"
+// }
+// let person2={
+//     nume:"Daniel",
+//     age:20,
+//     adresa:"Romania"
+// }
 
+
+// console.log(typeof(person));
+// console.log(person); //rezultatul este de tip jspn -javascript object notation
+// const hobbies=["pescuit la crap","inot","ciclism"];
+// const prices=[12.5,13.3,25.7,30.1];
+// console.log(hobbies);
+// const pescuit=hobbies[0];
+// console.log(pescuit);
+// console.log(prices[3]);
+
+
+// let persons=[person,person1,person2];
+// console.log(persons);
+// const people=[{id:1,age:35,email:"person@email.com"},{id:2,age:35,email:"person2@email.com"}]
+// console.log(people);
+//operatori
+const name="Mihai";
+// operatori aritmetici =,-,/,*,--,++
+//operatori de comparare <,>,==, !=
+//operatori logici or, and , !
+//operatorul === verifica si tipul variabilei, evaluand intai tipul variabilei
+let mynumber="12";
+let number1=12; let number2=23;
+
+console.log(mynumber==number1); //comnpara doar valoarea datelor
+console.log(mynumber===number1); //compara inclusiv tipul de date
+console.log(mynumber!==number1); //compara incusiv tipul de date
+console.log(number1>=number2);
+console.log(number1<=number2);
+
+
+
+
+
+
+
+
+// min 53
 
 
 
