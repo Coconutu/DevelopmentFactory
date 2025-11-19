a961fbcbb906f438a630bcf601c62606d3dddafe
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-19 11:50:47 +0200

    update

--- Clase si obiecte/index.js
+++ Clase si obiecte/index.js
@@ -1,4 +1,8 @@
+// let p2=new Person("Daniel","Sebastian",new Date("13-12-1983")); //da eroare deoarece clasele nu sunt hoisted
 class Person{
+  // #firstName; //facem campurile private cu diez in fata #firstName
+  // #lastName;
+  // #dateOfBirth;
   constructor(firstName,lastName,dateOfBirth){
     this.firstName=firstName;
     this.lastName=lastName;
@@ -11,4 +15,5 @@ class Person{
 }
 let p=new Person("Mihai","Popescu",new Date("12-12-2023"));
 console.log(p.calculeazaVarsta());
+
 // min 1:40
