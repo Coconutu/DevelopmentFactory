4062eb64ab68af0c4a83a8c1be3639cf1974e079
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-19 12:21:55 +0200

    update

---
+++ 6. Elemente HTML/index.js
@@ -0,0 +1,13 @@
+// var query=document.querySelectorAll(".page-title"); //selectare multipla
+var query=document.getElementById("title");
+// var query2=document.getElementsByClassName("page-title");
+
+// console.log(query.innerText);
+
+query.innerHTML +="<p id="text">Buna ziua</p>"
+//query.append("Buna ziua!");
+console.log(query.innerHTML);
+var hello=document.getElementById("text");
+//query.removeChild(hello);
+hello.style.color="red";
+// min 1:59
