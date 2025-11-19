46dee1f2809d4f136b0dfb14f8240ee6ea1dc0cb
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-19 10:59:32 +0200

    clase

--- Clase si obiecte/index.js
+++ Clase si obiecte/index.js
@@ -1,10 +1,13 @@
-class Person {
-    constructor(firstName, lastName, dateOfBirth) {
-      this.firstName=firstName;
-      this.lastName=lastName;
-      this.dateOfBirth=dateOfBirth;
+class Person{
+  constructor(firstName,lastName,dateOfBirth){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.dateOfBirth=dateOfBirth;
+  }
+    calculeazaVarsta(){
+      return new Date().getFullYear()-this.dateOfBirth.getFullYear();
     }
 
-    calculateAge() { }
 }
-// min 1:37
+let p=new Person("Mihai","Popescu",new Date("12-12-2023"));
+console.log(p.calculeazaVarsta());
