e486270e8cc024685e220bf47f9727832e6c1a46
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-11-20 14:59:07 +0200

    progress

---
+++ 7. Events/index.js
@@ -0,0 +1,6 @@
+var submitButton=document.getElementById("btnSubmit");
+submitButton.addEventListener("click",onBtnClick);
+function onBtnClick(event){
+    alert("Butonul a fost apasat");
+}
+//min 2:03
