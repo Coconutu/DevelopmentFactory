40dd1efe5d50bfa56cb5c83a0d4c65a972d663de
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-11-21 07:54:03 +0200

    myself

--- 8. Tasker/script.js
+++ 8. Tasker/script.js
@@ -1,7 +1,29 @@
-var taskInputText=document.getElementById("input-task").value;
+const submitBtn = document.querySelector(".submitBtn");
+submitBtn.addEventListener('click', doSubmitButton);
 
-const submitBtn=document.querySelector(".submitBtn");
-submitBtn.addEventListener('click',doSubmitButton);
-function doSubmitButton(event){
-alert(taskInputText);
-};
+function doSubmitButton(event) {
+    event.preventDefault();
+    var taskInputText = document.querySelector(".input-task").value;
+   if (taskInputText.trim() === "") {
+        alert("Please enter a task.");
+        return;
+    }   
+    const lista=document.querySelector(".lista");
+    const newTask = document.createElement("li");
+    newTask.textContent = taskInputText;
+    lista.appendChild(newTask);
+    newTask.classList.add("task-item");
+    newTask.addEventListener('click', function() {
+        newTask.textContent = "Completed: " + taskInputText;
+    
+        newTask.classList.add("completed-task");
+    });   
+    
+}
+const clearListBtn = document.querySelector(".clear-list-btn");
+clearListBtn.addEventListener('click', doClearListButton);
+function doClearListButton(event) {
+    event.preventDefault();
+    const lista=document.querySelector(".lista");
+    lista.innerHTML = '';
+}
